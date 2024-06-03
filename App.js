6332398f7978da246274d28431733b1bc5714b78
girish6j87 <girish6j87@gmail.com>
2024-06-03 23:49:05 +0530

    PS React-101 Day-02

--- App.js
+++ App.js
@@ -5,12 +5,13 @@
  *          <h2> I'm h2 tag </h2>
  *      </div>
  *      <div id="child2">
- *          <h1> I'm h1 tag </h1>
+ *          <h1> I'm h1 tag from parcel</h1>
  *          <h2> I'm h2 tag </h2>
  *      </div>
  * </div>
  */
-
+import React from "react";
+import ReactDOM from "react-dom/client"
 
 // const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"Hello World from React!");
 // console.log(heading);
@@ -23,7 +24,7 @@ const parent = React.createElement("div",{id:"parent"},
 [React.createElement("h1",{},"I'm an h1 tag"),
 React.createElement("h2",{},"I'm an h2 tag")  
 ]),React.createElement("div",{id:"child2    "},
-[React.createElement("h1",{},"I'm an h1 tag"),
+[React.createElement("h1",{},"I'm an h1 tag from parcel"),
 React.createElement("h2",{},"I'm an h2 tag")  
 ])])
 console.log(parent);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # PS React 101
+
+# Parcel
+- Dev build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - Written in c++
+- Caching - For Faster Builds
+- Image optimization
+- Minification of files also
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling
+- Diagnostic
+- Error Handling
+- HTTPS
+- Tree Shaking - will remove unsed code for you
+- Different dev and prod bundles
