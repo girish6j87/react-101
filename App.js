40106a4c8136ec531c2c50d75a2a3d3143fa7969
girish6j87 <girish6j87@gmail.com>
2024-06-05 01:07:54 +0530

    PS React-101 Day-03

--- App.js
+++ App.js
@@ -1,34 +1,77 @@
-/**
- * <div id="parent">
- *      <div id="child">
- *          <h1> I'm h1 tag </h1>
- *          <h2> I'm h2 tag </h2>
- *      </div>
- *      <div id="child2">
- *          <h1> I'm h1 tag from parcel</h1>
- *          <h2> I'm h2 tag </h2>
- *      </div>
- * </div>
- */
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"Hello World from React!");
+// React Element
+// const heading = React.createElement("h1",{id:"heading"},"PS React-101");
+
 // console.log(heading);
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
 
-// Due to the below complexity we need here JSX here
-const parent = React.createElement("div",{id:"parent"},
-[React.createElement("div",{id:"child"},
-[React.createElement("h1",{},"I'm an h1 tag"),
-React.createElement("h2",{},"I'm an h2 tag")  
-]),React.createElement("div",{id:"child2    "},
-[React.createElement("h1",{},"I'm an h1 tag from parcel"),
-React.createElement("h2",{},"I'm an h2 tag")  
-])])
-console.log(parent);
+//JSX : It is not html in javascript rather it is html or xml like syntax
+// JSX (transpiled before it reaches the JS) - Parcel - Babel
+// jsxHeading, heading are react element
+//JSX => Babel transpiles it to React.createElement => ReactElement-JS object => HTMlElement(render)
+// React Element
+const elem = <span>React Element </span>;
+
+// Title Component
+const Title1 = () => (
+  <h1 className="head" tabIndex="8">
+    PS React-101 using JSX with arrow function
+  </h1>
+);
+
+// OR
+
+const Title2 = function () {
+    return <h1 className="heading">React functional component with normal function </h1>;
+  };
+
+// React Component
+// Class Based Component - Old way of writing code
+// Functional Component - New way of writing code
+
+// What is react functional component : It is a normal javascript funtion which returns some piece of JSX.
+// Also we can say that it is a javascript function which returns a react element or jsx code
+const HeadingComponent1 = () => {
+  return <h1 className="heading">React functional component</h1>;
+};
+
+// OR
+const number = 1000;
+const HeadingComponent2 = () => (
+  <div id="container">
+    {/* component composition */}
+    <Title2/>
+    <Title1 /> 
+    <Title1></Title1>
+    {Title1()}
+     {number}
+     {/* {jsxHeading} */}
+     {/* writing javascript inside of jsx */}
+     <h2>{number}</h2>
+     <h2>{100+300}</h2>
+     {console.log("will this also works!")}
+    <h1 className="heading">React functional component</h1>
+  </div>
+);
+
+const jsxHeading = (
+    <h1 className="head" tabIndex="8">
+      {elem}
+      PS React-101 using JSX
+      <HeadingComponent2/>
+    </h1>
+  );
+  
+  console.log(jsxHeading);
+
+const fn1 = () => true;
+
+const fn2 = () => {
+  true;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+//root.render(jsxHeading);
+root.render(<HeadingComponent2 />);
